cbf96de1f05ee5fb4034b33995d071cb79fa2dc8
Ilario Dal Grande <ilario.dalgrande@mibit.it>
2019-03-05 18:11:58 +0100

    added postData & asyncPostData

--- index.js
+++ index.js
@@ -2,31 +2,66 @@ import axios from 'axios'
 
 export const fetchData = (url, actions, axiosInstance=null, successCallback=null) => {
   const {begin, success, failure} = actions
-  const axiosi = (
-	  axiosInstance === null ?
-	  axios :
-	  axiosInstance
-  )
+  const axiosi = axiosInstance === null ? axios : axiosInstance
   return dispatch => {
-    dispatch(fetchDataBegin(begin))
+    dispatch(dispatchDataBegin(begin))
     return axiosi.get(url)
     .then(json => {
       dispatch(
         (typeof successCallback === 'function') ?
           successCallback(success, json.data) :
-          fetchDataResut(success, json.data)
+          fetchDataResult(success, json.data)
       )
       return json.data
     })
-    .catch(error => dispatch(fetchDataResut(failure, error, true)))
+    .catch(error => dispatch(fetchDataResult(failure, error, true)))
   }
 }
 
-const fetchDataBegin = (type) => ({ type })
-const fetchDataResut = (type, data, isError=false) => {
+export const postData = (url, actions, axiosInstance=null, successCallback=null) => {
+  const {begin, success, failure} = actions
+  const axiosi = axiosInstance === null ? axios : axiosInstance
+  return dispatch => {
+    dispatch(dispatchDataBegin(begin))
+    return axiosi.get(url)
+    .then(json => {
+      dispatch(
+        (typeof successCallback === 'function') ?
+          successCallback(success, json.data) :
+          postDataResult(success, json.data)
+      )
+      return json.data
+    })
+    .catch(error => dispatch(postDataResult(failure, error, true)))
+  }
+}
+
+export async function asyncPostData(url, data, axiosInstance=null) {
+  return await asyncPostDataInner(url, data, axiosInstance)
+}
+
+const asyncPostDataInner = (url, data, axiosInstance=null) => {
+  const axiosi = axiosInstance === null ? axios : axiosInstance
+  axiosi.post(url, data)
+  .then(json => {
+    return json.data
+  })
+  .catch(error => error)
+}
+
+const dispatchDataBegin = (type) => ({ type })
+const fetchDataResult = (type, data, isError=false) => {
 	return (
 		isError ?
 		{ type, error:data } :
 		{ type, data }
 	)
 }
+const postDataResult = (type, data, isError=false) => {
+	return (
+		isError ?
+		{ type, error:data } :
+		{ type, data }
+	)
+}
+
